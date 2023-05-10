a646fe64d67a85cf122f2843cda6b34c675f8930
clemmzer <121194755+clemmzer@users.noreply.github.com>
2023-05-10 10:05:57 +0200

    structure player et monstre (#4)

---
+++ src/entities/player.h
@@ -0,0 +1,17 @@
+struct player
+{
+    int max_hp;
+    int hp;
+    int defense;
+    int attack;
+    int nbKey;
+};
+
+struct monstre
+{
+    int max_hp;
+    int hp;
+    int defense;
+    int attack;
+};
+
