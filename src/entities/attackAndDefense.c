b9d9be15f148138cc1767e935c5809042dedf600
clemmzer <121194755+clemmzer@users.noreply.github.com>
2023-05-10 11:27:01 +0200

    structure

---
+++ src/entities/attackAndDefense.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "entities.h"
+#include <math.h>
+
+
+int combat(player_t player, monster_t monster) {
+
+    double damageMonster = player.attack - monster.defense;
+    double damagePlayer = monster.attack - player.defense;
+
+    if (damageMonster <1 || damagePlayer<1) {
+        damageMonster = 1;
+    }
+    while( player.hp > 0 && monster.hp > 0) {
+        monster.hp = monster.hp - damageMonster;
+        if(monster.hp <= 0) {
+            return 1;
+        }
+        player.hp = player.hp - damagePlayer;
+        if(player.hp <= 0) {
+            return 0;
+        }
+    }
+
+
+}
+
+int main(){
+    
+}
+
+
