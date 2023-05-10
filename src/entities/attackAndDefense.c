84813b7f804200dca54e3a21c38b087a4bf68439
clemmzer <121194755+clemmzer@users.noreply.github.com>
2023-05-10 11:34:39 +0200

    ok

--- src/entities/attackAndDefense.c
+++ src/entities/attackAndDefense.c
@@ -10,10 +10,10 @@ int combat(player_t player, monster_t monster) {
     double damageMonster = player.attack - monster.defense;
     double damagePlayer = monster.attack - player.defense;
 
-    if (damageMonster <1 ) {
+    if (damageMonster < 1 ) {
         damageMonster = 1;
     }
-    if (damagePlayer <1 ) {
+    if (damagePlayer  < 1 ) {
         damagePlayer = 1;
     }
     while( player.hp > 0 && monster.hp > 0) {
@@ -28,10 +28,6 @@ int combat(player_t player, monster_t monster) {
     }
 
 
-}
-
-int main(){
-    
 }
 
 
