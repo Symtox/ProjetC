339c964fd52cdb530d939448680ff9fddfb16356
clemmzer <121194755+clemmzer@users.noreply.github.com>
2023-05-10 09:20:29 +0200

    test

---
+++ test.c
@@ -0,0 +1,16 @@
+int main(){
+    int a = 1;
+    int b = 2;
+    int c = 3;
+    int d = 4;
+    if(a == 1){
+        if(b == 2){
+            if(c == 3){
+                if(d == 4){
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
